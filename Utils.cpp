624fe9f02d4cf229cc7f33d9c970b39fcf560d1f
FinnWilliamsArm <Finn.Williams@Arm.com>
2019-12-06 17:12:42 +0000

    IVGCVSW-4211 Rename QuantisedSymm8 to QSymmS8
    !armnn:2421
    
    Signed-off-by: FinnWilliamsArm <Finn.Williams@Arm.com>
    Change-Id: I30da5235f6610306f9300b4532cf2c79ee8789f8

--- Utils.cpp
+++ Utils.cpp
@@ -122,7 +122,7 @@ armnn::TensorInfo GetTensorInfoForOperand(const V1_2::Operand& operand)
             type = armnn::DataType::QuantisedAsymm8;
             break;
         case V1_2::OperandType::TENSOR_QUANT8_SYMM:
-            type = armnn::DataType::QuantisedSymm8;
+            type = armnn::DataType::QSymmS8;
             break;
         case V1_2::OperandType::TENSOR_QUANT16_SYMM:
             type = armnn::DataType::QuantisedSymm16;
